5cecbf8fce10a5f7740a34ca52474fffdbe71305
muntasir-priyoshop <muntasir.priyoshop@gmail.com>
2025-10-21 13:36:50 +0600

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const serviceBaseUrl = 'http://localhost:3000';
-export const USER_SECRATE = 'soowgood@@2024';
+export const USER_SECRATE = 'prescriba@@2024';
 const apiUrl = 'https://steady-grateful-farms-temp.trycloudflare.com';
 export const authenticationApi =
   'https://cheaper-enter-losing-copied.trycloudflare.com';
@@ -9,17 +9,17 @@ export const prescriptionApi =
 export const environment = {
   production: false,
   application: {
-    name: 'SoowGoodWeb',
+    name: 'prescriba',
     logoUrl: '',
   },
   apis: {
     default: {
       url: apiUrl,
-      rootNamespace: 'SoowGoodWeb',
+      rootNamespace: 'prescriba',
     },
   },
   localization: {
-    defaultResourceName: 'SoowGoodWeb',
+    defaultResourceName: 'prescriba',
     languages: [
       {
         cultureName: 'en',
