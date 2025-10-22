202df28834b6c6d9e4f791e9e2e328069717235b
muntasir-priyoshop <muntasir.priyoshop@gmail.com>
2025-10-22 12:47:10 +0600

    update

--- src/app/features-modules/public/home/home.component.ts
+++ src/app/features-modules/public/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { DoctorDirectoryComponent } from './doctor-directory.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [DoctorDirectoryComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
